d90712c68ee9139da6c48d327ba2c66577777e0a
Hwang <jhwang@ccbcmd.edu>
2023-12-21 14:15:45 -0500

    started on day 2 part 2

---
+++ AdventOfCode/src/day2_part2/Day2_Part2.java
@@ -0,0 +1,11 @@
+package day2_part2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day2_Part2 {
+    public static void main(String[] args) {
+    
+    }
+}
